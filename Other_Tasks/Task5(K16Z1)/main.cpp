7e27110f5b517a381ff44f4d470770e25091578c
nazkord <nazkord@gmail.com>
2019-03-24 16:50:54 +0100

    add first part of the task

---
+++ Other_Tasks/Task5(K16Z1)/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+struct node {
+    double value;
+    node * next;
+};
+
+void printOut(node * A[100]) {
+    for(int i = 0; i < 100; i++) {
+        node * tmp = A[i];
+        while(tmp != nullptr) {
+            std::cout << tmp -> value << " ";
+            tmp = tmp ->next;
+        }
+        std::cout << "\n";
+    }
+}
+
+void sort(node * head) {
+
+    // declare auxiliary variable for head
+    node * phead = head;
+
+    //array of buckets
+    node * A[100];
+    //make all indicator NULL
+    for(int i = 0; i < 100; i++)
+        A[i] = nullptr;
+
+    while(phead != nullptr) {
+        node * tmp = new node;
+        tmp -> value = phead -> value;
+        tmp -> next = A[(int)((phead->value)*10)];
+        A[(int)((phead->value)*10)] = tmp;
+        phead = phead -> next;
+    }
+
+    printOut(A);
+}
+
+int main() {
+    node * first = nullptr;
+    for(int i = 0; i < 10; i++) {
+        node * tmp = new node;
+        std::cin >> tmp -> value;
+        tmp ->next = first;
+        first = tmp;
+    }
+
+    sort(first);
+
+    return 0;
+}
