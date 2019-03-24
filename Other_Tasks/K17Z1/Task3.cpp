d6119e7af0ed22234f5a0d7563c01a4adb338a4e
nazkord <nazkord@gmail.com>
2019-03-24 13:37:40 +0100

    Add task with first step made

---
+++ Other_Tasks/K17Z1/Task3.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+
+const int N = 6;
+
+struct node {
+    int group;
+    node * next;
+};
+
+void printOutList(node * first) {
+    while (first != nullptr) {
+        std::cout << first->group << " ";
+        first = first->next;
+    }
+    std::cout << "\n";
+}
+
+void printOutArray(int A[N/2]) {
+    for(int i = 0; i < N/2; i++)
+        std::cout << A[i] << " ";
+    std::cout << "\n";
+}
+
+int distanceToIdeal(node * head) {
+    //green team
+    int * A = new int[N/2];
+    int counterA = 0;
+    //blue team
+    int * B = new int[N/2];
+    int counterB = 0;
+
+    //first step
+    for(int i = 0; i < N; i++) {
+        if(head->group == 0) {
+            A[counterA++] = i+1;
+        }
+        else
+            B[counterB++] = i + 1;
+        head = head->next;
+    }
+
+    printOutArray(A);
+    printOutArray(B);
+
+    /* //second step
+    counterA = 0;
+    counterB = 0;
+    int mainCounter = 0;
+
+    while(counterA < N/2) {
+        if(A[counterA++] > N/2) {
+            while(counterB < N/2) {
+                if(B[counterB++] <= N/2)
+                    mainCounter = mainCounter + (B[counterB] - A[counterA])*2;
+            }
+        }
+    }
+
+    return mainCounter;*/
+
+    return  0;
+}
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    node * first = nullptr;
+    for(int i = 0; i < N; i++) {
+        node * tmp = new node;
+        std::cin>>tmp->group;
+        tmp->next = first;
+        first = tmp;
+    }
+
+    printOutList(first);
+
+    std::cout << distanceToIdeal(first);
+
+
+    return 0;
+}
