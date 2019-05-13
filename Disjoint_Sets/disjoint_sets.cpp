818f55e4b986fd2f71383c254735ee2e18cd6162
nazkord <nazkord@gmail.com>
2019-05-13 22:50:30 +0200

    Add make,find,union function

---
+++ Disjoint_Sets/disjoint_sets.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+const int T = 10;
+
+struct node {
+    node * parent;
+    int value;
+    int rank; /// for making union by rank (for better complexity)
+};
+
+node * makeSetTree(int key) {
+    node * newNode = new node;
+    newNode -> parent = newNode;
+    newNode -> value = key;
+    newNode -> rank = 0;
+    return newNode;
+}
+
+node * findSetTree(node * p) {  /// using path compression for better complexity
+    if(p -> parent != p)
+        p -> parent = findSetTree(p -> parent); /// find father of the tree and pin to p's father;
+    return p -> parent;
+}
+
+void unionTrees(node * x, node * y) {
+    node * reprOfX = findSetTree(x); /// repr - representant
+    node * reprOfY = findSetTree(y);
+    /// union using rank
+    if(reprOfX -> rank > reprOfY -> rank) {
+        reprOfY -> parent = reprOfX;
+    } else {
+        reprOfX -> parent = reprOfY;
+        if(reprOfX -> rank == reprOfY -> rank)
+            reprOfX -> rank += 1;
+    }
+}
+
+int main() {
+
+}
