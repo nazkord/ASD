e3cd6392b5bb00f12e49060be5390acae5767c25
nazkord <nazkord@gmail.com>
2019-05-13 23:11:36 +0200

    Solve typical problem using disjoint_set

--- Disjoint_Sets/disjoint_sets.cpp
+++ Disjoint_Sets/disjoint_sets.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 const int T = 10;
+const int n = 5;
+const int m = 5;
 
 struct node {
     node * parent;
@@ -34,6 +36,37 @@ void unionTrees(node * x, node * y) {
     }
 }
 
+bool roadToPort(int A[n][m]) {
+    node * setsArray[n][m];
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(A[i][j] > T) {
+                setsArray[i][j] = makeSetTree(A[i][j]);
+                /// union with left cell if possible
+                if(i > 0 && A[i-1][j] > T) {
+                    unionTrees(setsArray[i-1][j], setsArray[i][j]);
+                }
+                if(j > 0 && A[i][j-1] > T) {
+                    unionTrees(setsArray[i][j-1], setsArray[i][j]);
+                }
+
+            }
+        }
+    }
+
+    return (findSetTree(setsArray[0][0]) == findSetTree(setsArray[n-1][m-1]));
+}
+
 int main() {
+    int A[n][m] = {11,12,0,0,0,
+                   12,0,13,0,0,
+                   15,15,15,0,0,
+                   20,0,20,11,0,
+                   21,0,0,11,12};
+
+    if(roadToPort(A))
+        std::cout << "CAP IS THE BEST";
+    else
+        std::cout << "CAP SUCKS";
 
 }
