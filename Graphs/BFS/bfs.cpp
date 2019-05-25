eeb19d5d6b5ffd2af5a84bf76408812649b6cfee
nazkord <nazkord@gmail.com>
2019-05-25 21:04:54 +0200

    added init graph function

--- Graphs/BFS/bfs.cpp
+++ Graphs/BFS/bfs.cpp
@@ -22,7 +22,52 @@ struct graph {
     int size;
 };
 
+void insertToEndOfList(node * tail, int valueToInserted) {
+    node * newNode = new node;
+    newNode -> id = valueToInserted;
+    newNode -> next = nullptr;
+    tail -> next = newNode;
+}
+
+graph * graphInit() {
+    graph * graph1 = new graph;
+    int x = 0;
+
+    std::cout << "Enter number of vertex in graph: ";
+    std::cin >> graph1 -> size;
+    graph1 -> adjacent = new vertex *[graph1->size];
+
+    for(int i = 0; i < graph1->size; i++) {
+        graph1 -> adjacent[i] = new vertex;
+        graph1 -> adjacent[i] -> id = i;
+        graph1 -> adjacent[i] -> color = white;
+        node * p = graph1 -> adjacent[i]->neighbours = nullptr;
+
+        std::cout << "Enter the neighbor of vertex (end with -1) " << i << ": ";
+        while(true) {
+            std::cin >> x;
+            if(x == -1)
+                break;
+
+            if(p != nullptr) {
+                insertToEndOfList(p, x);
+                p = p -> next;
+            }
+            else {
+                p = new node;
+                p -> id = x;
+                p -> next = nullptr;
+            }
+        }
+    }
+
+    return graph1;
+}
+
 int main() {
+
+    graph * mainGraph = graphInit();
+
 
     return 0;
 }
